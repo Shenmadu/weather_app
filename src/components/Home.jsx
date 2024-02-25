50a39def76e5873aca4c4992ae777455e1b56ce0
shenmadu <tashen.madu@gmail.com>
2024-02-25 23:15:04 +0530

    add forcast for five days

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,7 +15,7 @@ export default function Home() {
 
 
     useEffect(() => {
-        fetch(`https://api.weatherapi.com/v1/current.json?key=007cf56f70d4431d99392717233012&q=${location}`)
+        fetch(`https://api.weatherapi.com/v1/forecast.json?key=007cf56f70d4431d99392717233012&q=${location}&days=4`)
             .then(response => response.json())
             .then(data => {
                 setWeather(data);
@@ -27,11 +27,11 @@ export default function Home() {
     return (
 
         <div className="container">
-         
+
 
             <div className="row img text-center" style={{ backgroundImage: `url(${backImg})` }} >
 
-                <h1 className="display-5 fw-bold text-body-emphasis mt-3" id="title">Weather Forcast</h1>
+                <h1 className="display-5 fw-bold text-body-emphasis mt-3" id="title">Weather Today</h1>
                 <div className="col-6 mx-auto">
                     <div className="d-flex" role="search">
                         <input id="txt" className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
@@ -40,8 +40,8 @@ export default function Home() {
                 </div>
             </div>
 
-            <div className=" row mt-2 text-center ">               
-           
+            <div className=" row mt-2 text-center ">
+
 
                 <div className="col-lg-4">
                     <h4>{weather.location && weather.location.name}</h4>
@@ -50,20 +50,73 @@ export default function Home() {
                 </div>
 
                 <div className="col-lg-4">
-                <h3>{weather.current && weather.current.temp_c} °C</h3>
-                <h4>Wind {weather.current && weather.current.wind_kph} kph</h4>
-                <h4>Wind Direction {weather.current && weather.current.wind_dir} </h4>
+                    <h3>{weather.current && weather.current.temp_c} °C</h3>
+                    <h4>Wind: {weather.current && weather.current.wind_kph} kph</h4>
+                    <h4>Wind Direction: {weather.current && weather.current.wind_dir} </h4>
+                    <h4>Pressure: {weather.current && weather.current.pressure_in} in</h4>
                 </div>
 
                 <div className="col-lg-4">
                     <h3><h4>{weather.location && weather.location.localtime}</h4></h3>
                     <h4>{weather.location && weather.location.country}</h4>
                     <h4>{weather.location && weather.location.region}</h4>
+
                 </div>
             </div>
 
+            {/* card section */}
 
-           
+            <div className="my-5">
+                <nav aria-label="breadcrumb">
+                    <div className="breadcrumb breadcrumb-chevron p-1 bg-success rounded-3  text-white ">
+                        <h4>Weather Forcast </h4>
+                    </div></nav>
+            </div>
+
+
+            <div className="row mx-auto">
+               
+                <div className="col">
+
+                    <div className="card text-center cardBox">
+                        <div className="card-body">
+                            <h5 className="card-title">{weather.forecast && weather.forecast.forecastday[0].day.condition.text}</h5>
+                            <h2>{weather.forecast && weather.forecast.forecastday[0].day.avgtemp_c}</h2>
+                            <img src={weather.forecast && weather.forecast.forecastday[0].day.condition.icon} alt=".." />
+                        </div>
+                    </div>
+
+                </div>
+                <div className="col">
+                    <div className="card text-center cardBox">
+                        <div className="card-body">
+                            <h5 className="card-title">{weather.forecast && weather.forecast.forecastday[1].day.condition.text}</h5>
+                            <h2>{weather.forecast && weather.forecast.forecastday[1].day.avgtemp_c}</h2>
+                            <img src={weather.forecast && weather.forecast.forecastday[1].day.condition.icon} alt=".." />
+                        </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card text-center cardBox">
+                        <div className="card-body">
+                            <h5 className="card-title">{weather.forecast && weather.forecast.forecastday[2].day.condition.text}</h5>
+                            <h2>{weather.forecast && weather.forecast.forecastday[2].day.avgtemp_c}</h2>
+                            <img src={weather.forecast && weather.forecast.forecastday[2].day.condition.icon} alt=".." />
+                        </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card text-center cardBox">
+                        <div className="card-body">
+                            <h5 className="card-title">{weather.forecast && weather.forecast.forecastday[0].day.condition.text}</h5>
+                            <h2>{weather.forecast && weather.forecast.forecastday[0].day.avgtemp_c}</h2>
+                            <img src={weather.forecast && weather.forecast.forecastday[0].day.condition.icon} alt=".." />
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+
 
         </div>
 
