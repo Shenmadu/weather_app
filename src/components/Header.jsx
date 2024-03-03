cdaaee1b1ef96da3f81b320b4210b1cdb7700b63
shenmadu <tashen.madu@gmail.com>
2024-03-03 22:17:24 +0530

    add icon to anv bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,28 @@
-
+import icon from '../assets/nav.png'
 export default function Header() {
 
     return (
         <div>
-        <div className="container-flex " style={{ backgroundColor: '#0e3065' }}>
-            <header className="d-flex flex-wrap justify-content-center py-2 border-bottom">
-                <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-                
-                </a>
-                <ul className="nav nav-pills">
-                    <li className="nav-item"><a href="#" className="nav-link text-white">Home</a></li>
-                    <li className="nav-item"><a href="#" className="nav-link text-white">Weather</a></li>
-                    <li className="nav-item"><a href="#" className="nav-link text-white">Forecast</a></li>
-                    <li className="nav-item"><a href="#" className="nav-link text-white">About</a></li>
-                </ul>
-            </header>
+            <nav className="navbar navbar-expand-lg " style={{ backgroundColor: '#0e3065' }}>
+                <div className="container-fluid">                   
+                    <img src={icon} style={{width:25,height:25}} alt="" />
+                    <button className="navbar-toggler" type="button" style={{color:'white'}} data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"  ></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav ms-auto ">
+                            <ul className="navbar-nav">
+                                <li className="nav-item"><a href="#" className="nav-link text-white">Home</a></li>
+                                <li className="nav-item"><a href="#" className="nav-link text-white">Weather</a></li>
+                                <li className="nav-item"><a href="#" className="nav-link text-white">Forecast</a></li>
+                                <li className="nav-item"><a href="#" className="nav-link text-white">About</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+           
         </div>
-    </div>
     )
 }
